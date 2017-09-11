23a1f5e3b1d5c1afe4c77eeab83e84d4b0d72b5e
crux wild <18210841568@163.com>
2017-09-11 16:30:20 +0800

    add

--- webpack.common.config.babel.js
+++ webpack.common.config.babel.js
@@ -6,6 +6,14 @@ export default {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          { loader: 'postcss-loader', options: { parser: 'sugarss', exec: true } },
+        ]
+      },
     ]
   },
 };
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,24 @@
 import common from './webpack.common.config.babel.js';
 
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
 export default {
   ...common,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader'
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
 };
