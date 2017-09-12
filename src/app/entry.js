b31ee3dfd24979b7f5c95656cc05168fffb20f23
crux wild <18210841568@163.com>
2017-09-12 19:03:24 +0800

    add

--- src/app/entry.js
+++ src/app/entry.js
@@ -1,10 +1,29 @@
+import './common.css';
+
 import ReactDOM from 'react-dom';
 
 import React from 'react';
 
-import './common.css';
+import { Provider } from 'react-redux';
+
+import { createStore, combineReducers } from 'redux';
+
+import { Router, Route, browserHistory } from 'react-router';
+
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+// Add the reducer to your store on the `routing` key
+const stroe = createStore(() => {});
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, stroe);
 
 ReactDOM.render(
-  <div>John</div>,
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
+  </Provider>,
   document.querySelector('main'),
 );
