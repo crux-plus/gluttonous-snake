7f2e1848b518bc00c9df1caf18f1803b07bd98ae
crux wild <18210841568@163.com>
2017-09-13 14:19:34 +0800

    add

--- src/ctrls/app.js
+++ src/ctrls/app.js
@@ -1,10 +1,31 @@
+// Fast, unopinionated, minimalist web framework for node.
 import express from 'express';
 
 import http from 'http';
 
+// Realtime application framework (Node.JS server)
 import socketIO from 'socket.io';
 
+// This is an Express view engine which renders React components on server.
+// It renders static markup and *does not* support mounting those views
+// on the client.
+import { createEngine } from 'express-react-views';
+
 const app = express();
+
+// set the engine
+app.engine('.jsx', engine);
+
+// set the view directory
+app.set('views', __dirname + '/views');
+
+// set jsx or js as the view engine
+// (without this you would need to supply the extension to res.render())
+// ex: res.render('index.jsx') instead of just res.render('index').
+app.set('view engine', 'jsx');
+
+// finally, set the custom view
+app.engine('jsx', createEngine());
 
 const server = http.createServer(app);
 
