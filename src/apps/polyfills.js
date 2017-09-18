e87a8f517d1b0d43c11c9f893d3ba3504703fb28
crux wild <18210841568@163.com>
2017-09-18 19:05:36 +0800

    add

--- src/apps/polyfills.js
+++ src/apps/polyfills.js
@@ -3,3 +3,8 @@ import 'babel-polyfill';
 
 // Isomorphic WHATWG Fetch API, for Node & Browserify.
 import 'isomorphic-fetch';
+
+// requestAnimationFrame polyfill library.
+import raf from 'raf';
+
+raf.polyfill();
