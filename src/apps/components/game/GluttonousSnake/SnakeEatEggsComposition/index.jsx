8277a718f7e67e0b9bc65ac0e69a6448750957c2
crux wild <18210841568@163.com>
2017-10-08 00:12:25 +0800

    add

--- src/apps/components/game/GluttonousSnake/SnakeEatEggsComposition/index.jsx
+++ src/apps/components/game/GluttonousSnake/SnakeEatEggsComposition/index.jsx
@@ -1,24 +1,12 @@
-import deepEqual from 'deep-equal';
-
+import React from 'react';
 
 import Status from '../Status';
-
-import Rtl from '../Rtl';
 
 import EggsLayer from './EggsLayer';
 
 import SnakeLayer from './SnakeLayer';
 
 import BackgroundLayer from './BackgroundLayer';
-
-import { Provider } from 'react-redux';
-
-import React from 'react';
-
-const Sym = Object.freeze({
-  STATUS: Symbol('status'),
-  BOUNDARY: Symbol('boundary'),
-});
 
 /**
  * @class
@@ -36,157 +24,13 @@ class SnakeEatEggsComposition extends React.PureComponent {
    */
   render() {
     return (
-      <Provider store={store}>
-        <div class="composition">
-          <SnakeLayer />
-          <EggsLayer />
-          <BackgroundLayer />
-        </div>
-      </Provider>
+      <div class="composition">
+        <SnakeLayer />
+        <EggsLayer />
+        <BackgroundLayer />
+      </div>
     );
   }
 }
 
-class SnakeEatEggs {
-  /**
-   * @constructor
-   */
-  constructor({ context=null, boundary=null, actions=null }) {
-    this.initStore();
-    this.initActions(actions);
-
-    this.initInstances(context);
-    this.bindSubscribe();
-
-    this.boundary = boundary;
-  }
-
-  /**
-   * @static
-   * @method
-   */
-  initStore() {
-    const middleware = [
-      translateEggs.bind(this),
-      correctionClean.bind(this),
-      boundaryDetection.bind(this),
-      collisionDetection.bind(this),
-      selfEatingDetection.bind(this),
-    ];
-    const store = createStore(
-      snakeEatEggs,
-      composeWithDevTools(
-        // other store enhancers if any
-        applyMiddleware(...middleware),
-      ),
-    );
-    this.store = store;
-  }
-
-  initActions(actions={}) {
-    const {
-      store: {
-        dispatch,
-      },
-    } = this;
-    const innerActions = bindActionCreators({
-      ...eggsActionCreators,
-      ...snakeActionCreators,
-      ...boundaryActionCreators,
-      ...snakeEatEggsActionCreators,
-    }, dispatch);
-    this.actions = {
-      ...actions,
-      ...innerActions,
-    };
-  }
-
-  /**
-   * @method
-   */
-  bindSubscribe() {
-    const {
-      store,
-      eggs,
-      snake,
-      background,
-    } = this;
-    store.subscribe(() => {
-      const state = store.getState().toJS();
-      eggs.mapStateToProps(state);
-      snake.mapStateToProps(state);
-      background.mapStateToProps(state);
-    });
-  }
-
-  /**
-   * @method
-   */
-  initInstances(context={}) {
-    const {
-      actions,
-    } = this;
-    this.snake = new Snake({ context, actions });
-    this.eggs = new Eggs({ context, actions });
-    this.background = new Background({ context, actions });
-    this.status = Status.PENDING;
-  }
-
-  /**
-   * @method
-   */
-  processStatus(status) {
-    switch (status) {
-      case Status.PENDING:
-        if (this.status === Status.END) {
-          this.actions.resetSnakeEatEggs();
-          this.eggs.translate();
-          this.actions.changeGameStatus({ status: Status.UNDERWAY });
-        }
-        break;
-    }
-  }
-
-  /**
-   * @method
-   */
-  set status(status) {
-    if (!deepEqual(this.status, status)) {
-      const {
-        snake,
-        eggs,
-      } = this;
-      snake.status = status;
-      eggs.status = status;
-
-      this.processStatus(status);
-      this[Sym.STATUS] = status;
-    }
-  }
-
-  /**
-   * @method
-   */
-  get status() {
-    return this[Sym.STATUS];
-  }
-
-  /**
-   * @method
-   */
-  set boundary(boundary) {
-    if (!deepEqual(this.boundary, boundary)) {
-      this[Sym.BOUNDARY] = boundary;
-      this.actions.resizeBoundary(boundary);
-    }
-  }
-
-  /**
-   * @method
-   */
-  get boundary() {
-    return this[Sym.BOUNDARY];
-  }
-}
-
-export default SnakeEatEggs;
+export default SnakeEatEggsComposition;
