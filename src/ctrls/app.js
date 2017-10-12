ba530266cf0701c4b6c91585b723eae6dfdd8831
crux wild <18210841568@163.com>
2017-10-12 22:53:44 +0800

    add

--- src/ctrls/app.js
+++ src/ctrls/app.js
@@ -1,6 +1,9 @@
 // Expressive middleware for node.js using ES2017 async functions.
 import Koa from 'koa';
 
+// Router middleware for koa.
+import Router from 'koa-router';
+
 // Authority node http serer package.
 import http from 'http';
 
