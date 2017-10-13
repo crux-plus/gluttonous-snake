67e17eee586abf52a72eb0ea13014b249a695cbd
crux wild <18210841568@163.com>
2017-10-13 18:44:39 +0800

    add

--- src/apps/polyfills.js
+++ src/apps/polyfills.js
@@ -1,6 +1,3 @@
-// Browser Polyfill for Babel.
-import 'babel-polyfill';
-
 // Isomorphic WHATWG Fetch API, for Node & Browserify.
 import 'isomorphic-fetch';
 
--- src/ctrls/app.js
+++ src/ctrls/app.js
@@ -1,8 +1,17 @@
 // Authority node http serer package.
 import http from 'http';
 
+// Authority node path package.
+import path from 'path';
+
 // Expressive middleware for node.js using ES2017 async functions.
 import Koa from 'koa';
+
+// Template rendering middleware for koa.
+import views from 'koa-render-view';
+
+// Development style logger middleware for koa.
+import logger from 'koa-logger';
 
 // All custome routes.
 import routers from './routers';
@@ -10,6 +19,7 @@ import routers from './routers';
 const app = new Koa();
 
 app
+  .use(logger())
   .use(routers.routes());
 
 http.createServer(app.callback()).listen(3000);
